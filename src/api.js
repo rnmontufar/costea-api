38fa1ab7d43c6c9675f012748308795744eef2c1
Rolfy Montufar <rolfymontufar@j643pkc69w.home>
2022-08-11 15:28:29 +0100

    Algoritmo funcionando

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const sc = require("./script");
 
 const app = express();
 const router = express.Router();
@@ -19,6 +20,13 @@ router.get("/test", (req, res) => {
 router.post("/testpost", (req, res) => {
   res.json({
     hello: "hit the POST!",
+  });
+});
+
+router.get("/cortes", (req, res) => {
+  sc.start();
+  res.json({
+    hello: "cortes!",
   });
 });
 
---
+++ src/script.js
@@ -0,0 +1,119 @@
+//events, buttons, etc, without optimizing algorithm
+const al = require("./algorithm");
+
+class cut {
+  constructor(length, amount) {
+    this.length = length;
+    this.amount = amount;
+  }
+}
+
+const start = () => {
+  let cutList = getInputList();
+  //maxCutLength
+  let maxCutLength = 60;
+
+  if (inputRepeatedLengths(cutList) == true) {
+    console.log("The lengths set for cutting cannot be repeated");
+    return;
+  }
+  if (inputExceedsMaxCutLength(cutList, maxCutLength) == true) {
+    console.log(
+      "At least one of the cutting lengths exceeds the specified material length"
+    );
+    return;
+  }
+  if (inputIsZeroOrNull(cutList, maxCutLength) == true) {
+    console.log("Lengths and quantities cannot be equal zero");
+    return;
+  }
+
+  cutList = cutList.sort(compareDesc);
+
+  al.algorithm(cutList, maxCutLength);
+}
+
+function inputIsZeroOrNull(myCutList, myMaxCutLength) {
+  for (i = 0; i < myCutListLength; i++) {
+    if (myCutList[i].length == 0) {
+      return true;
+    }
+    if (myCutList[i].amount == 0) {
+      return true;
+    }
+  }
+  if (myMaxCutLength == 0 || myMaxCutLength == null) {
+    return true;
+  }
+  return false;
+}
+
+function inputExceedsMaxCutLength(myCutList, myMaxCutLength) {
+  myCutListLength = myCutList.length;
+  for (i = 0; i < myCutListLength; i++) {
+    if (myCutList[i].length > myMaxCutLength) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function inputRepeatedLengths(myCutList) {
+  myCutListLength = myCutList.length;
+  //check if there are no repeated lengths
+  for (i = 0; i < myCutListLength; i++) {
+    let currentLength = myCutList[i].length;
+    for (j = 0; j < myCutListLength; j++) {
+      if (j != i) {
+        if (currentLength == myCutList[j].length) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+function getInputList() {
+  //Cantidad de cortes
+  let listLength = 3;
+  let cutLength;
+  let cutAmount;
+
+  let cutList = [];
+  cutList.length = 0;
+
+  let j = 0;
+  for (let i = 0; i < listLength; i++) {
+    cutLength = 2;
+    cutAmount = 3;
+		//cutLength = parseInt($("#input-length-" + i).val());
+    //cutAmount = parseInt($("#input-amount-" + i).val());
+
+    if (cutLength >= 0 && cutAmount >= 0) {
+      cutList[j] = new cut(cutLength, cutAmount);
+      //console.log('cut: '+j+", length: "+cutList[j].length+", amount: "+cutList[j].amount);
+      j++;
+    }
+  }
+	const cutList2 = [
+		{ length: 2, amount: 3 },
+		{ length: 3, amount: 4 },
+	]
+	console.log("cutList: ", cutList);
+	console.log("cutList2: ", cutList2);
+
+  return cutList2;
+}
+
+function compareDesc(a, b) {
+  if (a.length > b.length) {
+    return -1;
+  }
+  if (a.length < b.length) {
+    return 1;
+  }
+  return 0;
+}
+
+module.exports = { start };
